1d701b0607e84ce021434b5ba01171671b463a11
Diego Montebruni <montebrunidiego@gmail.com>
2024-07-22 13:27:00 -0300

    Configurado jackason e base para testes

---
+++ src/test/kotlin/com/montebruni/gastarmenos/configuration/BaseRestIT.kt
@@ -0,0 +1,21 @@
+package com.montebruni.gastarmenos.configuration
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.montebruni.gastarmenos.infrastructure.configuration.JacksonConfiguration
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Import
+import org.springframework.test.context.junit.jupiter.SpringExtension
+import org.springframework.test.web.servlet.MockMvc
+
+@Import(value = [JacksonConfiguration::class])
+@ExtendWith(SpringExtension::class)
+@Suppress("SpringJavaInjectionPointsAutowiringInspection")
+open class BaseRestIT {
+
+    @Autowired
+    lateinit var mockMvc: MockMvc
+
+    @Autowired
+    lateinit var mapper: ObjectMapper
+}
---
+++ src/test/kotlin/com/montebruni/gastarmenos/configuration/DatabaseIT.kt
@@ -0,0 +1,41 @@
+package com.montebruni.gastarmenos.configuration
+
+import org.junit.jupiter.api.BeforeEach
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+import org.springframework.test.context.DynamicPropertyRegistry
+import org.springframework.test.context.DynamicPropertySource
+import org.testcontainers.containers.PostgreSQLContainer
+
+@DataJpaTest
+@EnableJpaAuditing
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+open class DatabaseIT(
+    private val repositories: List<JpaRepository<*, *>> = emptyList()
+) {
+
+    constructor(repository: JpaRepository<*, *>) : this(listOf(repository))
+
+    @BeforeEach
+    fun cleanDb() = repositories.forEach { it.deleteAllInBatch() }
+
+    companion object {
+
+        @JvmStatic
+        val postgresContainer = PostgreSQLContainer("postgres:14.7-alpine").apply {
+            withUsername("app_gastar_menos")
+            withPassword("app_gastar_menos")
+            withDatabaseName("gastarmenos")
+        }.also { it.start() }
+
+        @JvmStatic
+        @DynamicPropertySource
+        fun registerDynamicProperties(registry: DynamicPropertyRegistry) {
+            registry.add("spring.datasource.url", postgresContainer::getJdbcUrl)
+            registry.add("spring.datasource.username", postgresContainer::getUsername)
+            registry.add("spring.datasource.password", postgresContainer::getPassword)
+        }
+    }
+}
